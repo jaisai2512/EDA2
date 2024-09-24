cc03880e0110f39f3979f4061bd3584471e64b60
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-24 17:25:14 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -1,5 +1,7 @@
 from API import api
 import Rules
+import json
+
 def summary_gen(df):
   summary = Rules.extract_data(df)
   prompt = f'''You are a data analyst with expertise in data interpretation using pandas dataframe. You are provided with a dataframe represented as a dictionary. Thoroughly analyze this data and provide a comprehensive summary in a single paragraph. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
@@ -12,13 +14,20 @@ def summary_gen(df):
   Here is the dictionary for analysis:{summary}
     '''
 
-  message = '''You are an experienced data analyst that can annotate datasets. Your instructions are as follows:
+  system_prompt = '''You are an experienced data analyst that can annotate datasets. Your instructions are as follows:
 i) ALWAYS generate the name of the dataset and the dataset_description
 ii) ALWAYS generate a field description.
 iii.) ALWAYS generate a semantic_type (a single word) for each field given its values e.g. company, city, number, supplier, location, gender, longitude, latitude, url, ip address, zip code, email, etc
 You must return an updated JSON dictionary without any preamble or explanation.
  Here is the dictionary for annonating:{summary}
 '''
-  summary = api(message)
+  messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "assistant", "content": f"""
+        Annotate the dictionary below. Only return a JSON object.
+        {summary}
+        """},
+        ]
+  summary = json.loads(api(message))
   return summary
   
