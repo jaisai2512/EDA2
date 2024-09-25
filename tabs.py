95c38a9e816d2f976b3b789b14e5617bd76e96ca
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-25 19:23:32 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -3,8 +3,10 @@ def Tabs(summary,df):
   l = []
   for i in summary['fields']:
     l.append(i['field_name'])
+  count = 0
   for j in st.tabs(l):
     with j:
       st.header("field_Name")
-      st.write(summary['fields'][0])
+      st.write(summary['fields'][count])
+      count = count +1
   
