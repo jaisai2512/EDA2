b328db69d2594204a61719333bc094ac38ae4bd4
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-25 19:14:15 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -1,1 +1,8 @@
-def Tabs(summary:json)
+def Tabs(summary,df):
+  l = []
+  for i in summary['fields']:
+    l.append(i['field_name'])
+  for j in st.tabs(l):
+    with j:
+      st.header("A cat")
+  
