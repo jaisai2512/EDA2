e195bd1d7dcf6869f51d7e24031fafdb80fb85fe
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-30 20:24:46 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -1,9 +1,10 @@
 from API import api
 import Rules
 import json
-
+import streamlit as st
 def summary_gen(df):
   rules = Rules.extract_data(df)
+  st.write(rules)
   prompt = f'''You are a data analyst with expertise in data interpretation using pandas dataframe. You are provided with a dataframe represented as a dictionary. Thoroughly analyze this data and provide a comprehensive summary in a single paragraph. The summary should be rich, compact, and concise, capturing all key information and insights from the data. Ensure that no critical details are omitted while keeping the text engaging.
 
   Please adhere to the following instructions:
