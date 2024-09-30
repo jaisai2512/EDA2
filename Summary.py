99a29a08ad8da70921690e34a1a9351dd766c040
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-09-30 19:20:29 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -20,6 +20,7 @@ ii) ALWAYS generate a field description.
 iii.) ALWAYS generate a semantic_type (a single word) for each field given its values e.g. company, city, number, supplier, location, gender, longitude, latitude, url, ip address, zip code, email, etc
 You must return an updated JSON dictionary without any preamble or explanation.
 iv) ALWAYS generate a field named DATA TYPE which specifies the data types of the column, the data type should be taken form the DICTIONARY.
+v) The DICTIONARY provided has mean  and no of null values add this in the json.
 '''
   messages = [
             {"role": "system", "content": system_prompt},
