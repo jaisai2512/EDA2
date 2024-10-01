e02bcbb31343ef14e2be3695fe5f486b3ffd7fde
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-02 01:07:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import os
 st.title("EDA Report Generator")
 
 # File uploader widget
-uploaded_file = st.file_uploader("Choose a CSV or pdf file")
+uploaded_file = st.file_uploader("Choose a CSV or pdf file",type=["csv", "pdf"])
 
 # Check if a file has been uploaded
 if uploaded_file is not None:
