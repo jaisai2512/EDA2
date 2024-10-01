669b7d3f594a9b82910997e869a27caefc040207
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-02 01:03:06 +0530

    Update PDF.py

--- PDF.py
+++ PDF.py
@@ -6,14 +6,14 @@ def pdf_parser(uploaded_file):
         os.makedirs(temp_dir)
 
     # Save the uploaded file temporarily to get the file path
-    file_path = os.path.join(temp_dir, uploaded_file.name)
-    with open(file_path, "wb") as f:    
+  file_path = os.path.join(temp_dir, uploaded_file.name)
+  with open(file_path, "wb") as f:    
         f.write(uploaded_file.getbuffer())
 
     # Display the file path
-    st.write("File saved at:", file_path)
-    additional_metadata = {'key': 'value'}
-    texts, metadata_list, langchain_docs = parse_doc_universal(file_path,additional_metadata,source_type, additional_metadata)
-    for i in langchain_docs:
+  st.write("File saved at:", file_path)
+  additional_metadata = {'key': 'value'}
+  texts, metadata_list, langchain_docs = parse_doc_universal(file_path,additional_metadata,source_type, additional_metadata)
+  for i in langchain_docs:
         st.write(i)
         exit()
