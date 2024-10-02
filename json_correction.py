570753e2c14ed14d2d0863771e3b86e50c686b15
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-02 19:00:27 +0530

    Update json_correction.py

--- json_correction.py
+++ json_correction.py
@@ -1,17 +1,12 @@
 from API import api
 def format_correction(summary,format):
-  system_prompt = "You are an expert in correcting and optimizing JSON outputs. Given the following JSON format, ensure that all fields have the correct data types and structure, maintaining the intended format."
+  system_prompt = "You are an expert in correcting the JSON output, Your main job to find any missing literals and correct them."
   messages = [
     {"role": "system", "content": system_prompt},
     {"role": "assistant", "content": f'''
     Here is the JSON that needs to be corrected:
     {summary}
-    
-    Please optimize this.
-    
-    The format the JSON needs to follow is:
-    {format}
-    Return Only the json.
+    Return Only the json object.
     '''}
 ]
   result = api(messages)
