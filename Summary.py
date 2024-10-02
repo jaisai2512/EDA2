aedbc2999a2b12b72bb6e96be2694235ed305bc8
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-02 18:53:58 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -54,7 +54,17 @@ Return the updated JSON dictionary directly, without any explanation.
     {template}
     """},
 ]
-  summary = json.loads(api(messages))
+  o_summary = api(messages)
+  while count <2:
+    try:
+      count = count +1
+      summary = json.loads(o_summary)
+      break
+    except:
+      o_summary = format_correction(o_summary,template)
+  if(count == 2):
+    st.write("reupload")
+    exit()
   system_prompt ='''As an experienced data analyst, your task is to create a structured dataset annotation based on the provided template. Follow these instructions:
       1. Fill in the dataset title and description accurately, ensuring clarity about the dataset's purpose and context.
       2. For each field in the dataset:
