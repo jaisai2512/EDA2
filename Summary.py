b93975d810d8bf7fa45815c3ec22002329cd32fe
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-16 12:49:20 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -22,7 +22,6 @@ def summary_gen(df):
       "Semantic_type": "string",
       "type_of_measurement": "string", 
       "mean": "number",  // INCLUDE ONLY IF APPLICABLE (NOT NULL)
-      "Skewness":"number", // INCLUDE ONLY IF APPLICABLE (NOT NULL)
       "number_of_nulls": "integer",
       "sample_elements": ["element_1", "element_2", "..."]  // A list of sample values for the field
     },
