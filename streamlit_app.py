2bd9c5c5a001b6d76cac6418078a6dd8b1be84eb
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-16 14:52:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ You are an experienced data analyst who generates a specified number of insightf
     st.write(data)
     for i in data:
         temp = df
-        system_prompt = f'''You are an expert data visualization person who knows to code well. You are given the following:\ni)Question: {i['question']}.\nii)Visualization Type: {i['visualization']}.\niii)Data: Provided in a DataFrame named temp.\niv)Summary of the data: {summary}.\nv) And a function to Complete.'''
+        system_prompt = f'''You are an expert data visualization person who knows to code well. You are given the following:\ni)Question: {i['question']}.\nii)Visualization Type: {i['visualization']}.\niii)Data: Provided in a DataFrame named temp.\niv)Summary of the data: {d_summary}.\nv) And a function to Complete.'''
         user_prompt = '''Your Objective is to Create a plan to improve and complete the plot_and_save(temp) function, which should:
                                     i) Come up with a optimal plan and used this plan to complete the function.
                                     ii)Ensure that the function handles and processes the input temp (which contains the data) efficiently.
