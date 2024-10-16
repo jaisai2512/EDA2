a72a38a4668f3751d51495088693791a9b49ba32
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-16 20:02:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ Remember to generate exactly five goals.
     st.write(data)
     for i in data:
         temp = df
-        system_prompt = f'''You are an expert data visualization person who knows to code well. You are given the following:\ni)Question: {i['question']}.\nii)Visualization Type: {i['visualization']}.\niii)Data: Provided in a DataFrame named temp.\niv)Summary of the data: {summary}.\nv) And a function to Complete.'''
+        system_prompt = f'''You are an expert data visualization person who knows to code well. You are given the following:\ni)Question: {i['question']}.\nii)Visualization Type: {i['visualization']}.\niii)Reason : {i['reason']}\niv)Data: Provided in a DataFrame named temp.\nv)Summary of the data: {summary}.\nvi) And a function to Complete.'''
         user_prompt = '''Your Objective is to Create a plan to improve and complete the plot_and_save(temp) function, which should:
                                     i) Come up with a optimal plan and used this plan to complete the function.
                                     ii)Ensure that the function handles and processes the input temp (which contains the data) efficiently.
