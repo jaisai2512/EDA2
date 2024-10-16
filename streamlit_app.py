a03fd90d41a30df6b9fc7aebb92f6f949a7a81ad
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-17 00:51:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ You are an experienced data analyst who generates a specified number of insightf
     st.write("New Information:")
 
     M_variate = f'''You are an expert data analyst. The user will provide a summary of a dataset, and your task is to generate goals that focus solely on the relationships between multiple variables and their interactions. From the summary, generate:
-        1)Questions: Based on the provided dataset SUMMARY generate valuable CORRELATION ANALYSIS questions 
+        1)Questions: Based on the provided dataset SUMMARY generate valuable MULTIVARITE ANALYSIS questions , you are allowed to perform anova
         2)Suggested Visualizations: Come up with a visualization which is the most effective way to visually express the question
         3)Rationale: Provide a rationale for the insights you expect to uncover through these questions and visualizations. Why are these questions and visualizations important for understanding the relationships between variables in the dataset? What key interactions or patterns do you hope to reveal using these techniques?
         Rules:
