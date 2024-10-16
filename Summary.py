15a50865ff10219f20325d2adbd47d15e6d3d1ac
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-16 13:17:36 +0530

    Update Summary.py

--- Summary.py
+++ Summary.py
@@ -85,7 +85,7 @@ When classifying, prioritize the semantic type and description to understand the
     {"role": "system", "content": system_prompt},
     {"role": "user", "content": f"""
     Please Create a summary based on the json given below:
-    {rules}
+    {o_summary}
     Output template:
     {temp}
     """},
