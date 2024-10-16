a4417adcf9faef4072290b3120b5664af0129d0c
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-16 18:54:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ You are an experienced data analyst who generates a specified number of insightf
     #exit()
     p_data = json.loads(api(messages))
     st.write(p_data)
-    Q_system_prompt = f'''You are a highly skilled data analyst. Your task is to evaluate the provided goals. If a goal is not appropriate, please propose a new one by replacing the old one , the new goal can either be an improved version of the previous one or a completely new goal that replaces the old one , make sure do not produce duplicate goals.
+    Q_system_prompt = f'''You are a highly skilled data analyst. Your task is to evaluate the provided goals. If a goal is not appropriate, please propose a new one by replacing the old one , the new goal can either be an improved version of the previous one or a completely new goal that replaces the old one .
     Ask the following questions to evaluate each goal:
     1)Is this an appropriate question to extract valuable information about a variable from the summary, or is there a better way to ask it?
     2)Does this goal provide any highly valuable information to the user?
