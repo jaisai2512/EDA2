e8f414a24c0d9b03eaf742e5bf9e9bb72c9c76f5
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-17 08:44:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,8 +75,8 @@ You are an experienced data analyst who generates a specified number of insightf
     
     M_variate = f'''You are a highly skilled data analyst. Based on the provided dataset summary, your task is to generate goals that focus solely on the relationships between multiple variables and their interactions. For each goal, include the following components:
 
-Questions: generate goal that explore potential relationships between the variable described and other fields in the dataset.your goals should be questioning the relationship between variable, no univariate analysis
-Suggested Visualizations: generate an appropriate visualization to help answer the question. Choose the visualization type that best represents the relationship between the variables involved
+Questions: Create goals that investigate potential relationships between the specified variable and other fields in the dataset, with a focus on questioning how these variables are related to one another (ex:What is the relationship between the variable x and variable y?)
+Suggested Visualizations: generate an appropriate visualization to help answer the question. Choose the visualization type that best represents the relationship between the variables involved(ex: box plot showing information)
 Rationale: Provide a rationale for the insights you expect to uncover through these questions and visualizations. Explain why these questions and visualizations are important for understanding the relationships between variables in the dataset. What key interactions or patterns do you hope to reveal using these techniques?
 Specify the variable used
 Can come up with 10 goals
