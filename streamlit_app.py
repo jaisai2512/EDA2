c6ccdf9ca332d2527c7af7a74a58ea2e0593bbbf
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-17 13:34:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ Rules:
     messages = [{"role": "system", "content": M_variate},{"role": "user","content":f"{user_prompt}"}]
     SYSTEM_INSTRUCTIONS = """You are a an experienced data analyst who can generate a given number of insightful GOALS about data, when given a summary of the data, and a specified persona. The VISUALIZATIONS YOU RECOMMEND MUST FOLLOW VISUALIZATION BEST PRACTICES (e.g., must use bar charts instead of pie charts for comparing quantities) AND BE MEANINGFUL (e.g., plot longitude and latitude on maps where appropriate). They must also be relevant to the specified persona. Each goal must include a question, a visualization (THE VISUALIZATION MUST REFERENCE THE EXACT COLUMN FIELDS FROM THE SUMMARY), and a rationale (JUSTIFICATION FOR WHICH dataset FIELDS ARE USED and what we will learn from the visualization). Each goal MUST mention the exact fields from the dataset summary above
 """
-    user_prompt = f"""The number of GOALS to generate is {n}. The goals should be based on the data summary below, \n\n .
+    user_prompt = f"""The number of GOALS to generate is 10. The goals should be based on the data summary below, \n\n .
         {summary} \n\n"""
     user_prompt += f"""\n The generated goals SHOULD BE FOCUSED ON THE INTERESTS AND PERSPECTIVE of A highly skilled data analyst who can come up with complex, insightful goals about data persona, who is insterested in complex, insightful goals about the data. \n"""
     messages = [
