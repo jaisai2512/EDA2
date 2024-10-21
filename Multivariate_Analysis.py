4ba1550a1873f44cfc5c161af5d8a4082576c885
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-22 00:19:50 +0530

    Update Multivariate_Analysis.py

--- Multivariate_Analysis.py
+++ Multivariate_Analysis.py
@@ -1,5 +1,6 @@
-class Multivariate:
-  def goal_generate(summary,FORMAT_INSTRUCTIONS):
+from API import api
+import json
+def goal_generate(summary,FORMAT_INSTRUCTIONS):
     M_variate = f'''You are a highly skilled data analyst. Based on the provided dataset summary, your task is to generate goals that focus solely on the relationships between multiple variables and their interactions. For each goal, include the following components:
 Questions: Create goals that investigate potential relationships between the variabels in the dataset , When creating the goals consider description , sample elements and type of measurement , Generate only  MULTIVARIATE ANALYSIS goals , For now DON'T CREATE GOALS which Involves Time(year,month,date) , ALWAYS allocate a goal for creating a correlation matrix between numerical variabels
 Suggested Visualizations: Suggest a single and most effective visualization for answer the question , Consider the type of measurement and description for suggesting the visualization and sample elements ,ollow these rules if the type of measurement is :\ni)continuous vs continuous use scatter plot\nii)discrete vs discrete use Grouped Bar plot\niii)discrete vs continuous use box plot or violin plot\niv)categorical vs categorical use Contingency Plot(Cross Tab)\n
