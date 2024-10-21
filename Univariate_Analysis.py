30e8a76ee04ab770c0fb4e4223d2ca872f62931c
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-21 22:33:41 +0530

    Update Univariate_Analysis.py

--- Univariate_Analysis.py
+++ Univariate_Analysis.py
@@ -9,7 +9,7 @@ class Univariate:
     Based on your evaluation:
     1)If the answers to questions are yes, then keep the goal unchanged.
     2)If the answer to any of the questions is no, modify the goal to make it optimal for univariate analysis.
-    Rules:The total number of goals should be only be 5
+    
     '''
 
     user_prompt = f'Evaluate and improve the goals\nGoals: {p_data}\n\nSummary of the Data: {summary}\n\n Rules :The newly generated, if any, should be based on univariate analysis only.\n{FORMAT_INSTRUCTIONS}'
