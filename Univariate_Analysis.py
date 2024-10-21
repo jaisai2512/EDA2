516e4b3d1023f5f78694db813644fd28f88259b0
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-21 22:14:59 +0530

    Update Univariate_Analysis.py

--- Univariate_Analysis.py
+++ Univariate_Analysis.py
@@ -2,7 +2,7 @@ import json
 from API import api
 class Univariate:
   def goal_enhancer(p_data,summary,FORMAT_INSTRUCTIONS):
-    Q_system_prompt = f'''You are a highly skilled data analyst. Your task is to evaluate the provided goals. If a goal is not appropriate, please propose a new one which replaces the old one , the new goal can either be an improved version of the previous one or a completely new goal.
+    Q_system_prompt = f'''You are a highly skilled data analyst. Your task is to evaluate the provided goals. If a goal is not appropriate, please propose a new one , the new goal can either be an improved version of the previous one or a completely new goal,Rember to remove the old goal.
     Ask the following questions to evaluate each goal:
     1)Is this an appropriate question to extract valuable information about a variable from the summary, or is there a better way to ask it?
     2)Does this goal provide any highly valuable information to the user?
