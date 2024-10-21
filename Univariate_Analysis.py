25a088aa8cd91f017425c27936750f0f6f840b6d
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-21 20:51:49 +0530

    Update Univariate_Analysis.py

--- Univariate_Analysis.py
+++ Univariate_Analysis.py
@@ -20,7 +20,7 @@ class Univariate:
 
     return data
     
-  def goal_generate(self,summary,FORMAT_INSTRUCTIONS):
+  def goal_generate(summary,FORMAT_INSTRUCTIONS):
     #System Prompt for the llm 
     U_SYSTEM_INSTRUCTIONS = f"""You are an expert data analyst. The user will provide a summary of a dataset, and your task is to generate goals which only focuses on the distribution and behaviour of the data. From the summary, generate\nQuestions:Based on the summary given ,What are the Univariate analsysis that can be asked which is highly valuable?\nSuggested Visualizations: Recommend the most effective visualizations (e.g., histograms, box plots) that would help analyze this variable. Explain why these visualizations are useful.\nRationale: Provide a rationale for the insights you expect to uncover through these visualizations and questions. Why do these questions and visualizations matter for understanding the dataset?\n\n Remeber Only generate five goals
     Rule:\ni)PLEASE AVOID THE VARIABLE WHICH HAS NO POTENTIAL OF HAVING DISTRIBUTION OR BEHAVIOUR(EX:ID)\n\n{FORMAT_INSTRUCTIONS}"""
@@ -34,7 +34,7 @@ class Univariate:
 
     u_goal_data = json.loads(api(messages))
 
-    return self.goal_enhancer(u_goal_data,summary,FORMAT_INSTRUCTIONS)
+    return u_goal_data
 
 
 
