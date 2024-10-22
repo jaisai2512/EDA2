af35c71f38216ec77930c13eeed365bbcaf96bcf
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2024-10-22 21:48:39 +0530

    Update Multivariate_Analysis.py

--- Multivariate_Analysis.py
+++ Multivariate_Analysis.py
@@ -1,5 +1,26 @@
 from API import api
 import json
+def goal_enhancer(p_data,summary,FORMAT_INSTRUCTIONS):
+    Q_system_prompt = f'''You are a highly skilled data analyst. Your task is to evaluate the provided goals. If a goal is not appropriate create a new goal and replace it with the old goal, 
+    Ask the following questions to evaluate each goal:
+    1)Is this an appropriate question to extract valuable information about a variable from the summary, or is there a better way to ask it?
+    2)Does this goal provide any highly valuable information to the user?
+    3)Does there are dupliates between the goals? 
+    Based on your evaluation:
+    1)If the answers to questions are yes, then keep the goal unchanged.
+    2)If the answer to any of the questions is no, generate a new goal to make it optimal for multivariate analysis .
+    '''
+
+    user_prompt = f'Evaluate and improve the goals\nGoals: {p_data}\n\nSummary of the Data: {summary}\n\n Rules :The newly generated, if any, should be based on multivariate analysis only.\n{FORMAT_INSTRUCTIONS}'
+
+    messages = [
+            {"role": "system", "content": Q_system_prompt},
+            {"role": "user","content":f"{user_prompt}" +"Make sure no duplicates are present between the goals and total goals should be only 5"}]
+
+    data = json.loads(api(messages))
+
+    return data
+
 def mul_goal_generate(summary,FORMAT_INSTRUCTIONS):
     M_variate = f'''You are a highly skilled data analyst. Based on the provided dataset summary, your task is to generate goals that focus solely on the relationships between multiple variables and their interactions. For each goal, include the following components:
 Questions: Create goals that investigate potential relationships between the variabels in the dataset , When creating the goals consider description , sample elements and type of measurement , Generate only  MULTIVARIATE ANALYSIS goals , For now DON'T CREATE GOALS which Involves Time(year,month,date) , ALWAYS allocate a goal for creating a correlation matrix between numerical variabels
@@ -17,4 +38,6 @@ Rules:
     messages = [{"role": "system", "content": M_variate},{"role": "user","content":f"{user_prompt}"}]
     data = json.loads(api(messages))
     return data
+
+
 
